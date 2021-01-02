86870db5c085c65ea281de0333ef934a35259f46
nullpenguin404 <66238042+nullpenguin404@users.noreply.github.com>
2021-01-02 17:29:09 +0900

    うｐ

--- js/aside.js
+++ js/aside.js
@@ -1,7 +1,6 @@
 $(function(){
   var loc = location.pathname,
       dir = loc.substring(0, loc.lastIndexOf('/')) + '/';
-  console.log(dir);
   if (!dir.match(/introduction/)) {
     $("aside").html(
       "<div class=\"sb-title\">\n\t\t\t<a href=\"introduction/introduction_index.html\">動画シリーズ</a>\n\t\t</div>\n\t\t<ul>\n\t\t\t<li><a href=\"introduction/カテゴリにつきmod一つ縛りで難関modに挑む.html\">カテゴリにつきmod一つ縛りで難関modに挑む(仮)</a></li>\n\t\t</ul>\n\t\t<div class=\"sb-title\">\n\t\t\tリンク集\n\t\t</div>\n\t\t<ul>\n\t\t\t<li><a href=\"remember.html\">覚書</a></li>\n\t\t\t<li><a href=\"contact.html\">ご意見ご感想</a></li>\n\t\t</ul>"
