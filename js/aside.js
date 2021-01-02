4336a3af0c0739a7f1c8bc79656a56333ff0744c
nullpenguin404 <66238042+nullpenguin404@users.noreply.github.com>
2021-01-02 18:54:36 +0900

    うｐ

--- js/aside.js
+++ js/aside.js
@@ -3,11 +3,11 @@ $(function(){
       dir = loc.substring(0, loc.lastIndexOf('/')) + '/';
   if (!dir.match(/introduction/)) {
     $("aside").html(
-      "<div class=\"sb-title\">\n\t\t\t<a href=\"introduction/introduction_index.html\">動画シリーズ</a>\n\t\t</div>\n\t\t<ul>\n\t\t\t<li><a href=\"introduction/カテゴリにつきmod一つ縛りで難関modに挑む.html\">カテゴリにつきmod一つ縛りで難関modに挑む(仮)</a></li>\n\t\t</ul>\n\t\t<div class=\"sb-title\">\n\t\t\tリンク集\n\t\t</div>\n\t\t<ul>\n\t\t\t<li><a href=\"remember.html\">覚書</a></li>\n\t\t\t<li><a href=\"contact.html\">ご意見ご感想</a></li>\n\t\t</ul>"
+      "<!--<div class=\"sb-title\">\n\t\t\t<a href=\"introduction/introduction_index.html\">動画シリーズ</a>\n\t\t</div>\n\t\t<ul>\n\t\t\t<li><a href=\"introduction/カテゴリにつきmod一つ縛りで難関modに挑む.html\">カテゴリにつきmod一つ縛りで難関modに挑む(仮)</a></li>\n\t\t</ul>-->\n\t\t<div class=\"sb-title\">\n\t\t\tリンク集\n\t\t</div>\n\t\t<ul>\n\t\t\t<li><a href=\"remember.html\">覚書</a></li>\n\t\t\t<li><a href=\"contact.html\">ご意見ご感想</a></li>\n\t\t</ul>"
     )
   } else if (dir.match(/introduction/)) {
     $("aside").html(
-      "<div class=\"sb-title\">\n\t\t\t<a href=\"introduction_index.html\">動画シリーズ</a>\n\t\t</div>\n\t\t<ul>\n\t\t\t<li><a href=\"カテゴリにつきmod一つ縛りで難関modに挑む.html\">カテゴリにつきmod一つ縛りで難関modに挑む(仮)</a></li>\n\t\t</ul>\n\t\t<div class=\"sb-title\">\n\t\t\tリンク集\n\t\t</div>\n\t\t<ul>\n\t\t\t<li><a href=\"../remember.html\">覚書</a></li>\n\t\t\t<li><a href=\"../contact.html\">ご意見ご感想</a></li>\n\t\t</ul>"
+      "<!--<div class=\"sb-title\">\n\t\t\t<a href=\"introduction_index.html\">動画シリーズ</a>\n\t\t</div>\n\t\t<ul>\n\t\t\t<li><a href=\"カテゴリにつきmod一つ縛りで難関modに挑む.html\">カテゴリにつきmod一つ縛りで難関modに挑む(仮)</a></li>\n\t\t</ul>-->\n\t\t<div class=\"sb-title\">\n\t\t\tリンク集\n\t\t</div>\n\t\t<ul>\n\t\t\t<li><a href=\"../remember.html\">覚書</a></li>\n\t\t\t<li><a href=\"../contact.html\">ご意見ご感想</a></li>\n\t\t</ul>"
     )
   }
 });
--- js/main_footer.js
+++ js/main_footer.js
@@ -3,11 +3,11 @@ $(function(){
       dir = loc.substring(0, loc.lastIndexOf('/')) + '/';
   if (!dir.match(/introduction/)) {
     $("footer.m-footer").html(
-      "\t\t<div class=\"f-logo\">\n\t\t\t<a href=\"index\.html\">nullpenguin404のお部屋</a>\n\t\t</div>\n\t\t<div class=\"f-list-l\">\n\t\t\t<ul>\n\t\t\t\t<li>${f-list}</li>\n\t\t\t\t<li>${f-list}</li>\n\t\t\t</ul>\n\t\t</div>\n\t\t\t<div class=\"f-list\">\n\t\t\t\t<ul>\n\t\t\t\t\t<li><img src=\"images/youtube\.png\" alt=\"\">youtube</li>\n\t\t\t\t\t<li><img src=\"images/twitter\.png\" alt=\"\">twitter</li>\n\t\t\t\t</ul>\n\t\t\t</div>"
+      "\t\t<div class= \"f-contents\">\t\t\t<div class=\"f-logo\">\n\t\t\t\t<a href=\"index\.html\">nullpenguin404のお部屋</a>\n\t\t\t</div>\n\t\t\t<div class=\"f-list-l\">\n\t\t\t\t<ul>\n\t\t\t\t\t<li>${f-list}</li>\n\t\t\t\t\t<li>${f-list}</li>\n\t\t\t\t</ul>\n\t\t\t</div>\n\t\t\t\t<div class=\"f-list\">\n\t\t\t\t\t<ul>\n\t\t\t\t\t\t<li><img src=\"images/youtube\.png\" alt=\"\">youtube</li>\n\t\t\t\t\t\t<li><img src=\"images/twitter\.png\" alt=\"\">twitter</li>\n\t\t\t\t\t</ul>\n\t\t\t</div>\n\t\t</div>"
     )
   } else if (dir.match(/introduction/)) {
     $("footer.m-footer").html(
-      "\t\t<div class=\"f-logo\">\n\t\t\t<a href=\"../index\.html\">nullpenguin404のお部屋</a>\n\t\t</div>\n\t\t<div class=\"f-list-l\">\n\t\t\t<ul>\n\t\t\t\t<li>${f-list}</li>\n\t\t\t\t<li>${f-list}</li>\n\t\t\t</ul>\n\t\t</div>\n\t\t\t<div class=\"f-list\">\n\t\t\t\t<ul>\n\t\t\t\t\t<li><img src=\"../images/youtube\.png\" alt=\"\">youtube</li>\n\t\t\t\t\t<li><img src=\"../images/twitter\.png\" alt=\"\">twitter</li>\n\t\t\t\t</ul>\n\t\t\t</div>"
+      "\t\t<div class=\"f-contents\">\t\t\t<div class=\"f-logo\">\n\t\t\t\t<a href=\"../index\.html\">nullpenguin404のお部屋</a>\n\t\t\t</div>\n\t\t\t<div class=\"f-list-l\">\n\t\t\t\t<ul>\n\t\t\t\t\t<li>${f-list}</li>\n\t\t\t\t\t<li>${f-list}</li>\n\t\t\t\t</ul>\n\t\t\t</div>\n\t\t\t\t<div class=\"f-list\">\n\t\t\t\t\t<ul>\n\t\t\t\t\t\t<li><img src=\"../images/youtube\.png\" alt=\"\">youtube</li>\n\t\t\t\t\t\t<li><img src=\"../images/twitter\.png\" alt=\"\">twitter</li>\n\t\t\t\t\t</ul>\n\t\t\t</div>\n\t\t</div>"
     )
   }
 });
