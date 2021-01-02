274d16500572d7387dabd90998c3b356753c6fb4
nullpenguin404 <66238042+nullpenguin404@users.noreply.github.com>
2021-01-02 17:22:09 +0900

    うｐ

---
+++ js/copylite.js
@@ -0,0 +1,5 @@
+$(function(){
+  $("footer.copylite").html(
+    "\t\t<small>Copylite &copy; nullpenguin404, all rights reserved.</small>"
+  )
+});
---
+++ js/main_footer.js
@@ -0,0 +1,13 @@
+$(function(){
+  var loc = location.pathname,
+      dir = loc.substring(0, loc.lastIndexOf('/')) + '/';
+  if (!dir.match(/introduction/)) {
+    $("footer.m-footer").html(
+      "\t\t<div class=\"f-logo\">\n\t\t\t<a href=\"index\.html\">nullpenguin404のお部屋</a>\n\t\t</div>\n\t\t<div class=\"f-list-l\">\n\t\t\t<ul>\n\t\t\t\t<li>${f-list}</li>\n\t\t\t\t<li>${f-list}</li>\n\t\t\t</ul>\n\t\t</div>\n\t\t\t<div class=\"f-list\">\n\t\t\t\t<ul>\n\t\t\t\t\t<li><img src=\"images/youtube\.png\" alt=\"\">youtube</li>\n\t\t\t\t\t<li><img src=\"images/twitter\.png\" alt=\"\">twitter</li>\n\t\t\t\t</ul>\n\t\t\t</div>"
+    )
+  } else if (dir.match(/introduction/)) {
+    $("footer.m-footer").html(
+      "\t\t<div class=\"f-logo\">\n\t\t\t<a href=\"../index\.html\">nullpenguin404のお部屋</a>\n\t\t</div>\n\t\t<div class=\"f-list-l\">\n\t\t\t<ul>\n\t\t\t\t<li>${f-list}</li>\n\t\t\t\t<li>${f-list}</li>\n\t\t\t</ul>\n\t\t</div>\n\t\t\t<div class=\"f-list\">\n\t\t\t\t<ul>\n\t\t\t\t\t<li><img src=\"../images/youtube\.png\" alt=\"\">youtube</li>\n\t\t\t\t\t<li><img src=\"../images/twitter\.png\" alt=\"\">twitter</li>\n\t\t\t\t</ul>\n\t\t\t</div>"
+    )
+  }
+});
