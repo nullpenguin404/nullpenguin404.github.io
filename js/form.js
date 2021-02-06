3d22f090b8a00b85ac749148629b5443aba38bfa
nullpenguin404 <66238042+nullpenguin404@users.noreply.github.com>
2021-02-06 19:10:50 +0900

    うｐ

---
+++ js/form.js
@@ -0,0 +1,75 @@
+'use stict';
+
+const whurl = 'https://discordapp.com/api/webhooks/807217443713318952/OpZv04y6AX9RMSebv2w_KSC9U_ULGuFsCQDsrXSH8RnUM5vwVZXPfll8GldP7iEJSt8N';
+
+$(function () {
+    $('button[type="submit"]').prop('disabled', true);
+    $('*:required').on('keyup', function () {
+        let flag = true;
+        $('*:required').each(function (e) {
+            if ($('*:required').eq(e).val() === '') {
+                flag = false;
+            }
+        });
+        if (flag) {
+            $('button[type="submit"]').prop('disabled', false);
+        } else {
+            $('button[type="submit"]').prop('disabled', true);
+        }
+    });
+    $('button[type="submit"]').on('click', function () {
+        const userName = getInputValues('input[name="user_name"]');
+        const returnName = getInputValues('input[name="return"]');
+        const title = getInputValues('input[name="title"]');
+        let desc = getInputValues('textarea[name="desc"]');
+
+        if (userName === '' || desc === '') {
+            return;
+        }
+
+        if (returnName !== '') {
+            desc = desc + '\n返信先 : ' + returnName;
+        }
+
+        const msg = {
+            "username": userName,
+            "embeds": [
+                {
+                    "title": title,
+                    "description": desc,
+                    "timestamp": new Date(),
+                }
+            ],
+            "content": "<@725683207655129098> "
+        }
+
+        fetch(whurl + "?wait=true",
+            {
+                "method": "POST",
+                "headers": { "content-type": "application/json" },
+                "body": JSON.stringify(msg)
+            }
+        );
+
+        displaySucsess();
+    });
+});
+
+/**
+ * Returns selected HTML element's value.
+ *
+ * @param {string} selecter
+ * @return {string} 
+ */
+function getInputValues(selecter) {
+    const value = $(selecter).val();
+    return value;
+}
+
+/**
+ * Displays sucsess dialog.
+ *
+ */
+function displaySucsess() {
+    document.querySelector('dialog').showModal();
+}
--- js/parts.js
+++ js/parts.js
@@ -1,10 +1,5 @@
 'use strict';
 
-const local = window.location.pathname;
-const path = local.substring(0, local.lastIndexOf('/'));
-
-console.log(path);
-
 $(function () {
     $('header').load('/parts/header.html');
     $('footer').load('/parts/footer.html');
