aa60a99a5c4eb2e4de338c457984929948bdd127
nullpenguin404 <66238042+nullpenguin404@users.noreply.github.com>
2021-02-07 18:57:09 +0900

    うｐ

--- js/form.js
+++ js/form.js
@@ -3,27 +3,42 @@
 const whurl = 'https://discordapp.com/api/webhooks/807217443713318952/OpZv04y6AX9RMSebv2w_KSC9U_ULGuFsCQDsrXSH8RnUM5vwVZXPfll8GldP7iEJSt8N';
 
 $(function () {
-    let flag = true;
+    let flag = false;
     $('button[type="submit"]').prop('disabled', true);
-    $('input[name="return"]').on('keyup', function () {
-        if ($(this).val() !== '') {
-            $('div#return_type').addClass('active');
+    $('input[name="return_type"]').change(function () {
+        if (typeof $('input[name="return_type"]:checked').val() === 'undifined') {
+            flag = false;
         } else {
-            $('div#return_type').removeClass('active');
+            flag = true;
+        }
+        $('*:required').each(function (e) {
+            if ($('*:required').eq(e).val() === '') {
+                flag = false;
+            } else {
+                flag = true;
+            }
+        });
+        if (flag) {
+            $('button[type="submit"]').prop('disabled', false);
+        } else {
+            $('button[type="submit"]').prop('disabled', true);
         }
     });
     $('*:required, input[name="return"]').on('keyup', function () {
         $('*:required').each(function (e) {
             if ($('*:required').eq(e).val() === '') {
                 flag = false;
-            }
-        });
-        if ($('input[name="return"]').val() !== '') {
-            if (typeof $('input[name="return_type"]').val() === 'undefined') {
-                flag = false;
             } else {
                 flag = true;
             }
+        });
+        if ($('input[name="return"]').val() !== '') {
+            $('div#return_type').addClass('active');
+            flag = false;
+        } else {
+            $('div#return_type').removeClass('active');
+            $('input[name="return_type"]').prop('checked', false);
+            flag = true;
         }
         if (flag) {
             $('button[type="submit"]').prop('disabled', false);
@@ -31,21 +46,10 @@ $(function () {
             $('button[type="submit"]').prop('disabled', true);
         }
     });
-    $('input[name="return_type"]').on('change', function () {
-        if ($('input[name="return"]').val() !== '') {
-            if (typeof $('input[name="return_type"]').val() === 'undefined') {
-                flag = false;
-                console.log(flag);
-            } else {
-                flag = true;
-                console.log(flag);
-            }
-        }
-    });
     $('button[type="submit"]').on('click', function () {
         const userName = getInputValues('input[name="user_name"]');
         const returnName = getInputValues('input[name="return"]');
-        const returnType = getInputValues('input[name="return_type]:checked');
+        const returnType = getInputValues('input[name="return_type"]:checked');
         const title = getInputValues('input[name="title"]');
         let desc = getInputValues('textarea[name="desc"]');
 
