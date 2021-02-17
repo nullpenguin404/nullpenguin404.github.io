0a75e6eb2ef94dd4a0a34db9fbc2e417852f58f3
nullpenguin404 <66238042+nullpenguin404@users.noreply.github.com>
2021-02-17 16:44:43 +0900

    うｐ

--- js/form.js
+++ js/form.js
@@ -1,105 +1,83 @@
-'use stict';
-
 const whurl = 'https://discordapp.com/api/webhooks/807217443713318952/OpZv04y6AX9RMSebv2w_KSC9U_ULGuFsCQDsrXSH8RnUM5vwVZXPfll8GldP7iEJSt8N';
 
-$(function () {
-    let flag = false;
+const cl = console.log;
+
+$(() => {
     $('button[type="submit"]').prop('disabled', true);
-    $('input[name="return_type"]').change(function () {
-        if (typeof $('input[name="return_type"]:checked').val() === 'undifined') {
-            flag = false;
-        } else {
-            flag = true;
-        }
-        $('*:required').each(function (e) {
-            if ($('*:required').eq(e).val() === '') {
-                flag = false;
-            } else {
-                flag = true;
-            }
-        });
-        if (flag) {
+    $('*:required').on('keyup', () => {
+        const user_name = $('input[name="user_name"]').val();
+        const content = $('textarea[name="desc"]').val();
+        $('input[name="return"]').trigger('keyup');
+        if (user_name !== '' && content !== '') {
             $('button[type="submit"]').prop('disabled', false);
         } else {
             $('button[type="submit"]').prop('disabled', true);
         }
     });
-    $('*:required, input[name="return"]').on('keyup', function () {
-        $('*:required').each(function (e) {
-            if ($('*:required').eq(e).val() === '') {
-                flag = false;
-            } else {
-                flag = true;
-            }
-        });
-        if ($('input[name="return"]').val() !== '') {
+    $('input[name="return"]').on('keyup', () => {
+        const return_name = $('input[name="return"]').val();
+        if (return_name !== '') {
             $('div#return_type').addClass('active');
-            flag = false;
+            $('button[type="submit"]').prop('disabled', true);
         } else {
             $('div#return_type').removeClass('active');
-            $('input[name="return_type"]').prop('checked', false);
-            flag = true;
+            $('button[type="submit"]').prop('disabled', false);
+            $('input[name="return_type"]:checked').prop('checked', false);
         }
-        if (flag) {
+    });
+    $('input[name="return_type"]').change(() => {
+        const return_type = $('input[name="return_type"]:checked').val();
+        if (typeof return_type !== 'undefined') {
             $('button[type="submit"]').prop('disabled', false);
         } else {
             $('button[type="submit"]').prop('disabled', true);
         }
     });
-    $('button[type="submit"]').on('click', function () {
-        const userName = getInputValues('input[name="user_name"]');
-        const returnName = getInputValues('input[name="return"]');
-        const returnType = getInputValues('input[name="return_type"]:checked');
-        const title = getInputValues('input[name="title"]');
-        let desc = getInputValues('textarea[name="desc"]');
-
-        if (userName === '' || desc === '') {
-            return;
-        }
-
-        if (returnName !== '') {
-            desc = desc + '\n\n' + returnType + ' : ' + returnName;
-        }
-
-        const msg = {
-            "username": userName,
-            "embeds": [
-                {
-                    "title": title,
-                    "description": desc,
-                    "timestamp": new Date(),
-                }
-            ]//,
-            //"content": "<@725683207655129098>"
-        }
-
-        fetch(whurl + "?wait=true",
-            {
-                "method": "POST",
-                "headers": { "content-type": "application/json" },
-                "body": JSON.stringify(msg)
-            }
-        );
-
-        displaySucsess();
+    $('button[type="submit"]').on('click', () => {
+        const user_name = $('input[name="user_name"]').val();
+        const content = $('textarea[name="desc"]').val();
+        const return_name = $('input[name="return"]').val();
+        const return_type = $('input[name="return_type"]:checked').val();
+        const title = $('input[name="title"]').val();
+        send(user_name, return_name, return_type, title, content);
+        document.querySelector('dialog').showModal();
+        setTimeout(() => {
+            window.location = 'contact.html'
+        }, 10000);
     });
 });
 
 /**
- * Returns selected HTML element's value.
+ * Send to discord.
  *
- * @param {string} selecter
- * @return {string} 
+ * @param {string} user_name
+ * @param {string} return_name
+ * @param {string} return_type
+ * @param {string} title
+ * @param {string} content
  */
-function getInputValues(selecter) {
-    const value = $(selecter).val();
-    return value;
-}
-
-/**
- * Displays sucsess dialog.
- *
- */
-function displaySucsess() {
-    document.querySelector('dialog').showModal();
+function send(user_name, return_name, return_type, title, content) {
+    if (return_name !== '') {
+        content = content + '\n\n' + return_type + ' : ' + return_name;
+    }
+    const msg = {
+        "username": user_name,
+        "embeds": [
+            {
+                "title": title,
+                "description": content,
+                "timestamp": new Date()
+            }
+        ]/*,
+            "content": "<@725683207655129098>"*/
+    }
+    fetch(whurl + "?wait=true",
+        {
+            "method": "POST",
+            "headers": { "content-type": "application/json" },
+            "body": JSON.stringify(msg)
+        })
+        .catch((reason) => {
+            throw 'error!\n' + reason;
+        });
 }
