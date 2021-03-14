2786a47bd6de6af8b0bc5415987cd43833d326e5
nullpenguin404 <66238042+nullpenguin404@users.noreply.github.com>
2021-03-14 11:02:07 +0900

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# nullpenguin404のお部屋
-nullpenguin404のホームページです。  
-主が作ったものとかおいてます。よろしく。  
-
-# License
-The source code is licensed GNU.  
-###### Copylight (c) 2021 Nullpenguin404
--- js/parts.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-$(function () {
-    $('header').load('/parts/header.html');
-    $('footer').load('/parts/footer.html');
-    $('aside').load('/parts/aside.html');
-});
